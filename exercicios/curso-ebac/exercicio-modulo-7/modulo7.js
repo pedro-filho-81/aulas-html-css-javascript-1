34d8aeba362ccf278cec5e66f3e1bd20f73e48b4
Pedro Filho <psfilho81@gmail.com>
2023-11-27 19:01:36 -0300

    Atualizações Exercício módulo-7

--- exercicios/curso-ebac/exercicio-modulo-7/modulo7.js
+++ exercicios/curso-ebac/exercicio-modulo-7/modulo7.js
@@ -1,25 +1,16 @@
-/* FAZ A LIGAÇÃO COm O HTmL */
+/* FAZ A LIGAÇÃO COM O FORMULÁRIO HTmL */
 const frm = document.querySelector('form');
 const numero1 = document.querySelector('#icampo1');
 const numero2 = document.querySelector('#icampo2');
 
+/* EXIBE UMA MENSAGEM PARABENIZANDO QUANDO O VALOR CORRESPONDE AO QUE FOI PEDIDO NO QUESTIONÁRIO */
 let mensagem = document.querySelector('#resposta');
 
-function validaNumero(n1, n2) {
-   if(n1 < n2) {
-      return 1;
-   } else {
-      return 0;
-   }
-}
-
-/* AO CLICAR NO BOTÃO DO FORmULÁRIO */
+/* AO CLICAR NO BOTÃO SUBMIT DO FORmULÁRIO */
 frm.addEventListener('submit', function(e) {
 
-   let valorValido = validaNumero(numero1.value, numero2.value);
-
    /* se o valor válido for igual a 1 */
-   if(valorValido == 1) {
+   if( numero1.value < numero2.value) {
       /* exibe isso */
       mensagem.innerHTML = `Parabéns! Você digitou ${numero1.value} e ${numero2.value} <br>O segundo valor é o maior.`; 
    } else { /* se não */
@@ -27,6 +18,7 @@ frm.addEventListener('submit', function(e) {
       mensagem.innerHTML = `Você errou! Você digitou ${numero1.value} e ${numero2.value} <br> o primeiro valor é maior, <br>ou os valores são iguais.`;
    }
    
+   /* LIMPA OS CAMPOS */
    numero1.value = '';
    numero2.value = '';
 
