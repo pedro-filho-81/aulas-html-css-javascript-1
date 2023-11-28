6b27536a1ab68dd049b9257608769cf945cd2ae2
Pedro Filho <psfilho81@gmail.com>
2023-11-28 12:18:42 -0300

    Introdução JQuery
    Primeiro exemplo

---
+++ exemplos/livro-aplicacao-web/cap04/projects/exemplo1/app.js
@@ -0,0 +1,7 @@
+let main = function() {
+   "use strict"; /* sempre usar essa string como primeira linha de código */
+   window.alert('Hello world!');
+};
+
+/* chama a função main */
+$(document).ready(main);
