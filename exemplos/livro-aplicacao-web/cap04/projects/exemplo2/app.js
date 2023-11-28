f6dfd2e19d1be9f2bea1567799ab58fd1de70353
Pedro Filho <psfilho81@gmail.com>
2023-11-28 17:15:17 -0300

    Update app.js

--- exemplos/livro-aplicacao-web/cap04/projects/exemplo2/app.js
+++ exemplos/livro-aplicacao-web/cap04/projects/exemplo2/app.js
@@ -1,6 +1,12 @@
 let main = function() {
    "use strict"; /* sempre usar essa string como primeira linha de código */
+
+   /* quando o usuário clicar em +, exiba “Hello World!” no console. */
    $(".comment-input button").on("click", function(event){
+      /* Abaixo dizemos: Na seção da classe comments adicione no final dos páragrafos o novo comentário */
+      $(".comments").append("<p>This is a new comment</p>");
+
+      /* No dev-tools console imprima Hello World! */
       console.log("Hello World!");
    });
 };
