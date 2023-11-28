10b14bba2556389099722b2ba8bb61c278337c02
Pedro Filho <psfilho81@gmail.com>
2023-11-28 18:26:33 -0300

    Update app.js

--- exemplos/livro-aplicacao-web/cap04/projects/exemplo2/app.js
+++ exemplos/livro-aplicacao-web/cap04/projects/exemplo2/app.js
@@ -1,13 +1,20 @@
 let main = function() {
    "use strict"; /* sempre usar essa string como primeira linha de código */
 
-   /* quando o usuário clicar em +, exiba “Hello World!” no console. */
+   /* quando o usuário clicar no botão ( + ) */
    $(".comment-input button").on("click", function(event){
-      /* Abaixo dizemos: Na seção da classe comments adicione no final dos páragrafos o novo comentário */
-      $(".comments").append("<p>This is a new comment</p>");
+      
+      // cria a variável que recebe a tag parágrafo do Html
+      
+      let $new_comment = $("<p>"),
+      /* .text($(".comment-input input").val());
+      */
+      comment_text = $(".comment-input input").val();
 
-      /* No dev-tools console imprima Hello World! */
-      console.log("Hello World!");
+      $new_comment.text(comment_text);
+
+      $(".comments").append($new_comment);
+      
    });
 };
 
