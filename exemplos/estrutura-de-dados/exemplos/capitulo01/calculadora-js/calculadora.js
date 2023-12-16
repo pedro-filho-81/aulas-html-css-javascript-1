7bfbd905ff45b665274afda012e7d39e41ea5a9c
Pedro Filho <psfilho81@gmail.com>
2023-12-16 18:32:18 -0300

    Create calculadora.js

---
+++ exemplos/estrutura-de-dados/exemplos/capitulo01/calculadora-js/calculadora.js
@@ -0,0 +1,8 @@
+
+<script src="#"><script src="debug.js">
+   <script src="hide.js"></script></script>Hello  
+   <button onclick="hide(this,true); 
+   debug('hide button 1');">Hide1</button>
+   <button onclick="hide(this); 
+   debug('hide button 2');">Hide2</button>
+</script>
