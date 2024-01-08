ec27b16fca002bb9d30ccba1adaf70f8cb7846d4
Pedro Filho <psfilho81@gmail.com>
2024-01-08 16:21:17 -0300

    Create ex3_1.js

---
+++ exemplos/algoritimo-com-js/capitulo03/exemplos/soma2numeros/ex3_1.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")(); // adiciona paacote para entrada de dados
+const num1 = Number(prompt('Digite o 1º número: ')); // lê o primeiro número
+const num2 = Number(prompt('2º núvero: '));
+
+// calcula a soma
+const soma = num1 + num2;
+
+// exibe a soma
+console.log(`A soma de ${num1} + ${num2} é ${soma}`);
