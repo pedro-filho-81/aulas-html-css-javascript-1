910cc3a1c81360fb03c86b01fdd9c73137046c25
Pedro Filho <psfilho81@gmail.com>
2024-01-25 11:02:31 -0300

    Duplicados

---
+++ exemplos/js_definitivo/funcao-Math/math.js
@@ -0,0 +1,22 @@
+// FUNÇÃO MATH
+console.log(`Math.pow(2,53) - 2 elevado à potência 53 = ${Math.pow(2,53)}`); // => 9007199254740992: 2 elevado à potência 53  
+console.log(`Math.round(0.6) - arredonda 0,6 para o inteiro mais próximo = ${Math.round(.6)} `);   // => 1.0: arredonda para o inteiro mais próximo  
+console.log(`Math.ceil(0.6) - arredonda 0,6 para cima para um inteiro = ${Math.ceil(0.6)}`);  // => 1.0: arredonda para cima para um inteiro  
+console.log(`Math.floor(0.6) - arredonda 0,6 para baixo para um inteiro = ${Math.floor(0.6)}`); // => 0.0: arredonda para baixo para um inteiro  
+console.log(`Math.abs(-5) - valor absoluto de -5 é ${Math.abs(-5)}`)// => 5: valor absoluto  
+let x = 5;
+let y = 10;
+let z = 15;
+console.log(`let x = 5;\nlet y = 10;\nlet z = 15;`)
+console.log(`Math.max(x,y,z) - Retorna o maior argumento entre x, y, z = ${Math.max(x,y,z)}`)   // Retorna o maior argumento  
+console.log(`Math.min(x,y,z) - Retorna o menor argumento entre x, y, z = ${Math.min(x,y,z)}`)   // Retorna od menor argumento  
+console.log(`Math.random() - Número pseudoaleatório x, onde 0 <= x < 1.0 = ${Math.random()}`)// Número pseudoaleatório x, onde 0 <= x < 1.0  
+console.log(`Math.PI - π: circunferência de um círculo/ diâmetro = ${Math.PI}`)// π: circunferência de um círculo/ diâmetro  
+console.log(`Math.E - π: circunferência de um círculo/ diâmetro = ${Math.E}`)// e: A base do logaritmo natural  
+console.log(`Math.sqrt(3) - A raiz quadrada de 3 = ${Math.sqrt(3)}`)   // A raiz quadrada de 3  
+console.log(`Math.pow(3, 1/3) - A raiz cúbica de 3 = ${Math.pow(3, 1/3)}`)  // A raiz cúbica de 3  
+console.log(`Math.sin(0) - Trigonometria: também Math.cos, Math.atan = ${Math.sin(0)}`) // Trigonometria: também Math.cos, Math.atan,  
+console.log(`Math.log(10) - Logaritmo natural de 10 = ${Math.log(10)}`) // Logaritmo natural de 10  
+console.log(`Math.log(100) / Math.LN10 - Logaritmo de base 10 de 100 = ${Math.log(100) / Math.LN10}`)// Logaritmo de base 10 de 100  
+console.log(`Math.log(512) / Math.LN2 - Logaritmo de base 2 de 512 = ${Math.log(512) / Math.LN2}`)// Logaritmo de base 2 de 512  
+Math.exp(3) // Math.E ao cubo 
