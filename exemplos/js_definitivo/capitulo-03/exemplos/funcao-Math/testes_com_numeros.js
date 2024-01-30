03a05ef4dc217242cca79dec4aec4219dde29514
Pedro Filho <psfilho81@gmail.com>
2024-01-30 11:00:28 -0300

    Create testes_com_numeros.js

---
+++ exemplos/js_definitivo/capitulo-03/exemplos/funcao-Math/testes_com_numeros.js
@@ -0,0 +1,8 @@
+var x = .3 - .2;
+console.log('var x = .3 - .2;');
+console.log('x = ' + x);
+let y = .2 - .1;
+console.log('let y = .2 - .1;');
+console.log('y = ' + y);
+x == y;
+console.log('x == Y: ' + (x == y));
