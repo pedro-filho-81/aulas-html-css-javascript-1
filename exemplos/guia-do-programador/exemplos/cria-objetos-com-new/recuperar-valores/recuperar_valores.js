cbb42e4f47353607380bbcf0c39d7d1d299abeed
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-22 12:46:42 -0300

    Create recuperar_valores.js

---
+++ exemplos/guia-do-programador/exemplos/cria-objetos-com-new/recuperar-valores/recuperar_valores.js
@@ -0,0 +1,59 @@
+ // NEW cria o objeto vazio livro
+ let livro = new Object();
+
+ // variáveis
+ let message = "";
+ let propriedade;
+ 
+ // LIVRO.TITULO cria a propriedade TITULO do objeto LIVRO
+ // e atribui um VALOR, aqui o  valor é uma STRING
+ livro.titulo = "Ajax Com JQuery"
+ // cria a propriedade autor e atribui uma string
+ livro.autor = "Maurício Samy Silva"
+ // a pripriedade página recebe o valor 432
+ livro.pagina = 432;
+ // a propriedade preço recebe uma string
+ livro.preço = "R$ 69,00";
+ // cria a função freteSedex para o objeto livro
+ 
+ // redefine o valor da propriedade preço
+ livro.preço = "R$ 72,00";
+ 
+ livro.freteSedex = function(cepOrigem, cepDestino, peso ){
+    // variável
+    let valorDoFrete = "12345";
+    // script de cálculo do frete
+    return valorDoFrete;
+ } // final 
+ 
+ // cria diversas propriedades para o objeto livro e atribui varias strings
+ livro.capitulo1 = "Revisão do AJAX";
+ livro.capitulo2 = "Funções para requisições AJAX";
+ livro.capitulo3 = "Eventos e misceânnea";
+ livro.capitulo4 = "Requisições XmL";
+ livro.capitulo5 = "Introdução ao formato JSON";
+ livro.capitulo6 = "Requisições JSON";
+ 
+ // EXIBE OS VALORES DO OBJETO LIVRO
+ for(propriedade in livro) {
+    // message, variável que recebe as propriedes e os valores
+    // livro[propriedades] objeto indexado
+    // O sinal de ( + ) serve para concatenação  
+    message += propriedade + " : " + livro[propriedade] + "\n";
+ }
+ // exibe todas as mensagens
+ alert(`${message}`);
+
+ /* OUTRA mANEIRA PARA RECUPERAR VALORES DE PROPRIEDADES, mÉTODOS E VARIÁVEIS DE Um OBJETO. */
+
+// variável recebe a string do objeto livro que está na propriedade autor
+let nomeDoAutor = livro.autor;
+
+// variável recebe a string do objeto livro que está na propriedade capitulo5
+let capituloCinco = livro.capitulo5;
+
+// variável recebe o valor que está no método function do objeto livro
+let valorDoFrete = livro.freteSedex(22050047, 69190120, 1060);
+
+// exibe os valores
+alert(`Nova maneira de exibir\n Autor: ${nomeDoAutor}\n CapítuloCinco: ${capituloCinco}\n valorDoFrete R$: ${valorDoFrete}`);
